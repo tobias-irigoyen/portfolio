da02518d2bdf161ba23ea3004ea354fb85cf1e94
tobias-irigoyen <tobias.irigoyen@nayra.coop>
2022-08-11 19:32:42 -0300

    clean react app default structure. Create first components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import Work from './components/Work';
+import About from './components/About';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
